d453b2c8aaa6b68fba3f60544c063fa719722898
komo15 <komododo15@gmail.com>
2020-01-05 14:40:38 +0900

    メールアドレスをポスト送信するコード作成

---
+++ sample1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>メアド入力</title>
+</head>
+<body>
+<p>メールアドレスを入力してみてね</p>
+<form action="sample2.php" method="post">
+<input type="text" size="50" name="mail"><br>
+<input type="submit" value="送信">
+</form>
+</body>
+</html>
